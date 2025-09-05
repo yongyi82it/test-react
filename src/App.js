a38397d4580ed6029d33bfdbd1101a6829807358
yongyi82it <flytorei71@gmail.com>
2025-09-05 17:21:35 +0900

    수정

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import TOC from './components/TOC';
-import Content from './components/Content';
+import ReadContent from './components/ReadContent';
+import CreateContent from './components/CreateContent';
 import Subject from './components/Subject';
 import Control from './components/Control';
 import './style.css';
@@ -33,10 +34,11 @@ class App extends Component {
   }
 
   render() {
-    var _title, _desc = null;
+    var _title, _desc, _article = null;
     if(this.state.mode === 'welcome'){
       _title = this.state.welcome.title;
       _desc = this.state.welcome.desc;
+      _article = <ReadContent title={_title} disc={_desc}></ReadContent>;
     }else if(this.state.mode === 'read'){
       var i=0;
       while(i<this.state.contents.length){
@@ -48,6 +50,9 @@ class App extends Component {
         }
         i=i+1;
       }
+      _article = <ReadContent title={_title} disc={_desc}></ReadContent>;
+    }else if(this.state.mode === 'create'){
+      _article = <CreateContent></CreateContent>
     }
 
     return (
@@ -72,7 +77,8 @@ class App extends Component {
           }.bind(this)} 
           data={this.state.contents}
         ></TOC>
-        <Content title={_title} disc={_desc}></Content>
+        
+        {_article}
       </div>
     );
   }
