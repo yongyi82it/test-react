e229ed82594633af643acf6b006f4ba56ad5b32e
yongyi82it <flytorei71@gmail.com>
2025-09-08 17:20:28 +0900

    수정

--- src/App.js
+++ src/App.js
@@ -20,6 +20,7 @@ function App() {
 class App extends Component {
   constructor(props){
     super(props);
+    this.max_content_id = 3;
     this.state = {
       mode:'create',
       selected_content_id:2,
@@ -53,7 +54,17 @@ class App extends Component {
       _article = <ReadContent title={_title} disc={_desc}></ReadContent>;
     }else if(this.state.mode === 'create'){
       _article = <CreateContent onSubmit={function(_title,_desc){
-      console.log(_title,_desc);
+      this.max_content_id = this.max_content_id + 1;
+      //this.state.contents.push(
+      //  {id:this.max_content_id, title:_title, desc:_desc}
+      //);
+      var _contents = this.state.contents = this.state.contents.concat(
+        {id:this.max_content_id, title:_title, desc:_desc}
+      )
+      this.setState({
+        contents:_contents
+      })
+        console.log(_title,_desc);
       }.bind(this)}></CreateContent>
     }
 
